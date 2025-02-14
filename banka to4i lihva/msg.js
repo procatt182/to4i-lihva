7d41f75d98fa267dd5f673ce2487642681279187
procatt182 <emoepinkleaf@gmail.com>
2025-02-14 17:03:07 +0200

    Update msg.js

--- banka to4i lihva/msg.js
+++ banka to4i lihva/msg.js
@@ -1,21 +1,26 @@
 function sendToDiscord() {
     const webhookURL = "https://discord.com/api/webhooks/1339929906489987134/21uRpsYRpZeyHF-vBzQqTzWosgihRvuSQAPdcCp_WLiz6CPVvXReW7qOxdEZFcOOGoiY";
     const name = document.getElementById("ime").value;
-    const amount = document.getElementById("amount").value;
+    const number = document.getElementById('number').value;
+    let amount = parseFloat(document.getElementById("amount").value);
     
-    if (!name || !amount) {
-        alert("Please fill out all fields.");
+    if (!name || isNaN(amount)) {
+        alert("napishi neshto ve pedal.");
         return;
     }
-    
+
+    const newAmount = amount * 1.1;
+    const numberToSend = number.trim() === "" ? "Няма номер" : number;
+
     let segashna_data = new Date();
     let sro4na_data = new Date();
     sro4na_data.setDate(segashna_data.getDate() + 7);
-    let data = sro4na_data.getDate().toString().padStart(2, '0') + "/" + (sro4na_data.getMonth() + 1).toString().padStart(2, '0') + "/" + sro4na_data.getFullYear();
+    let data = (sro4na_data.getMonth() + 1).toString().padStart(2, '0') + "/" + sro4na_data.getDate().toString().padStart(2, '0') + "/" + sro4na_data.getFullYear();
 
     const payload = {
-        content: `кредит на ${name}\n**Име:** ${name}\n**Сума:** ${amount} лв\n**Срок:** ${data}`
+        content: `# **кредит на ${name}**\n**Име:** ${name}\n**Номер:** ${numberToSend}\n**Сума:** ${newAmount.toFixed(2)} лв\n**Срок:** ${data}\n----------------------------------------------------`
     };
+
     fetch(webhookURL, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
